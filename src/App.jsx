b1e6f3fcbf5fb4b9d851743227debd2ea40daff1
Nazam <nazam98-be@email.com>
2020-01-21 12:37:36 +0100

    ajustements mineurs

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import Garage from './components/Garage';
 
 const App = () => {
     const [liste, setListe] = useState([
-        { nom: 'Mercedes Benz S 260', an: '1991', couleur: 'Bleu', conso: 'Essence', valide: 'Non validé' },
-        { nom: 'Fiat 500', an: '2010', couleur: 'Noire', conso: 'Essence', valide: 'Non validé' },
-        { nom: 'Volkswagen Crafter', an: '2016', couleur: 'Blanche', conso: 'Diesel', valide: 'Validé' },
-        { nom: 'BMW 114', an: '2012', couleur: 'Bleu', conso: 'Essence', valide: 'Non validé' },
-        { nom: 'Audi A3', an: '2015', couleur: 'Rouge', conso: 'Diesel', valide: 'Validé' }
+        { nom: 'Mercedes Benz S 260', annee: '1991', couleur: 'Bleu', type: 'Essence', valide: 'Non validé' },
+        { nom: 'Fiat 500', annee: '2010', couleur: 'Noire', type: 'Essence', valide: 'Non validé' },
+        { nom: 'Volkswagen Crafter', annee: '2016', couleur: 'Blanche', type: 'Diesel', valide: 'Validé' },
+        { nom: 'BMW 114', annee: '2012', couleur: 'Bleu', type: 'Essence', valide: 'Non validé' },
+        { nom: 'Audi A3', annee: '2015', couleur: 'Rouge', type: 'Diesel', valide: 'Validé' }
     ]);
     return (
-        <div>
+        <div className='text-center'>
+            <h1 className='my-5 text-info'>Concessionnaire Random</h1>
             <Garage liste={liste} />
         </div>
     )
--- src/components/Vehicule.jsx
+++ src/components/Vehicule.jsx
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
+
 const Vehicule = (props) => {
+
     let info = [];
+
     const [valid, setValid] = useState(props.info['valide']);
+
     for (let key in props.info) {
         info.push(key);
     }
 
     return (
+
         <div className={'my-3 col text-center p-2 ' + (valid === 'Non validé' && 'border border-danger')}>
+
             <ul className='text-left'>
+
                 {info.map((e, index) => <li key={index}>{e + ' : ' + (e !== 'valide' ? props.info[e] : valid)}</li>)}
+
             </ul>
-            {valid === 'Non validé' && <button onClick={() => { setValid('Validé') }}>Valider</button>}
+
+            {valid === 'Non validé' && <button className='btn btn-success' onClick={() => { setValid('Validé') }}>Valider</button>}
+
         </div>
+
     )
 };
 
 // valid === 'Non validé' ? setValid('Validé') : setValid('Non validé')
-// Cette ligne permet de switcher entre les deux états
+// Cette ligne permet de switcher entre les deux états, Elle doit être placé dans l'écouteur de click du bouton
 
 export default Vehicule;
